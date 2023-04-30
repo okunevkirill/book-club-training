4ce6b53c0cddd2e067c103728f93186c0d414b08
ok_kir <clicktmin@gmail.com>
2023-04-30 22:11:38 +0400

    feat: Add code samples for chapter 10

---
+++ book_club_training/python_concurrency_with_asyncio/chapter010/listing_10_10.py
@@ -0,0 +1,22 @@
+import asyncio
+from listing_10_9 import retry, TooManyRetries
+
+
+async def main():
+    async def always_fail():
+        raise Exception("А я грохнулась!")
+
+    async def always_timeout():
+        await asyncio.sleep(1)
+
+    try:
+        await retry(always_fail, max_retries=3, timeout=0.1, retry_interval=0.1)
+    except TooManyRetries:
+        print("Слишком много попыток!")
+    try:
+        await retry(always_timeout, max_retries=3, timeout=0.1, retry_interval=0.1)
+    except TooManyRetries:
+        print("Слишком много попыток!")
+
+
+asyncio.run(main())
---
+++ book_club_training/python_concurrency_with_asyncio/chapter010/listing_10_6.py
@@ -0,0 +1,40 @@
+import functools
+from aiohttp import web
+from aiohttp.web_request import Request
+from aiohttp.web_response import Response
+from listing_10_4 import DB_KEY, create_database_pool, destroy_database_pool
+from base import SETTING
+
+
+routes = web.RouteTableDef()
+
+
+@routes.get("/users/{id}/cart")
+async def time(request: Request) -> Response:
+    try:
+        str_id = request.match_info["id"]
+        user_id = int(str_id)
+        db = request.app[DB_KEY]
+        favorite_query = "SELECT product_id from user_cart where user_id = $1"
+        result = await db.fetch(favorite_query, user_id)
+        if result is not None:
+            return web.json_response([dict(record) for record in result])
+        raise web.HTTPNotFound()
+    except ValueError:
+        raise web.HTTPBadRequest()
+
+
+app = web.Application()
+app.on_startup.append(
+    functools.partial(
+        create_database_pool,
+        host=SETTING.POSTGRES_HOST,
+        port=SETTING.POSTGRES_PORT,
+        user=SETTING.POSTGRES_USER,
+        password=SETTING.POSTGRES_PASSWORD,
+        database="cart",
+    )
+)
+app.on_cleanup.append(destroy_database_pool)
+app.add_routes(routes)
+web.run_app(app, port=8003)

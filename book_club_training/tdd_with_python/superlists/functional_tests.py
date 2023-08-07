20e652a266c8a75611ee113e97491de94b755ba8
ok_kir <clicktmin@gmail.com>
2023-08-07 17:10:21 +0400

    feat [tdd_with_python]: FT now checks for the ability to enter a list item

--- book_club_training/tdd_with_python/superlists/functional_tests.py
+++ book_club_training/tdd_with_python/superlists/functional_tests.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
-import unittest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
+import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -25,19 +27,29 @@ class NewVisitorTest(unittest.TestCase):
         # Она видит, что заголовок и шапка страницы
         # говорят о списках неотложных дел
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Закончить тест!")
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-DO", header_text)
 
         # Ей сразу же предлагается ввести элемент списка
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(input_box.get_attribute("placeholder"), "Enter a to-do item")
 
         # Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби –
         # вязание рыболовных мушек)
+        input_box.send_keys("Купить павлиньи перья")
 
         # Когда она нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertTrue(any(row.text == "1: Купить павлиньи перья" for row in rows))
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев" (Эдит очень методична)
-
+        self.fail("Закончить тест!")
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальный URL-адрес – об этом

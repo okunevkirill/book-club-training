8d173f0346ddc6739e40718416b915b93313d9f1
ok_kir <clicktmin@gmail.com>
2023-08-08 00:10:53 +0400

    feat [tdd_with_python]: After restructuring the test

--- book_club_training/tdd_with_python/superlists/functional_tests.py
+++ book_club_training/tdd_with_python/superlists/functional_tests.py
@@ -17,6 +17,12 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(3)
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """Подтверждение строки в таблице списка"""
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """Тест: можно начать список и получить его позже"""
 
@@ -42,10 +48,7 @@ class NewVisitorTest(unittest.TestCase):
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn("1: Купить павлиньи перья", [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Купить павлиньи перья")
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев" (Эдит очень методична)
@@ -55,12 +58,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn("1: Купить павлиньи перья", [row.text for row in rows])
-        self.assertIn(
-            "2: Сделать мушку из павлиньих перьев", [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table("1: Купить павлиньи перья")
+        self.check_for_row_in_list_table("2: Сделать мушку из павлиньих перьев")
 
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальный URL-адрес – об этом

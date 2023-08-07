35944e233ab5cf64f244c9584df5ef27463b29a9
ok_kir <clicktmin@gmail.com>
2023-08-07 17:51:40 +0400

    feat [tdd_with_python]: Homepage HTML is now generated from the template

--- book_club_training/tdd_with_python/superlists/functional_tests.py
+++ book_club_training/tdd_with_python/superlists/functional_tests.py
@@ -28,7 +28,7 @@ class NewVisitorTest(unittest.TestCase):
         # говорят о списках неотложных дел
         self.assertIn("To-Do", self.browser.title)
         header_text = self.browser.find_element(By.TAG_NAME, "h1").text
-        self.assertIn("To-DO", header_text)
+        self.assertIn("To-Do", header_text)
 
         # Ей сразу же предлагается ввести элемент списка
         input_box = self.browser.find_element(By.ID, "id_new_item")
@@ -45,7 +45,10 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertTrue(any(row.text == "1: Купить павлиньи перья" for row in rows))
+        self.assertTrue(
+            any(row.text == "1: Купить павлиньи перья" for row in rows),
+            "Новый элемент списка не появился в таблице",
+        )
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев" (Эдит очень методична)

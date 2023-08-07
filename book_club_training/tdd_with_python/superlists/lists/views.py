c3ae94fc7f4d77e011578eb52a41c2e51a231e83
ok_kir <clicktmin@gmail.com>
2023-08-08 00:01:35 +0400

    feat [tdd_with_python]: Before restructuring the test

--- book_club_training/tdd_with_python/superlists/lists/views.py
+++ book_club_training/tdd_with_python/superlists/lists/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home_page(request):
     """Домашняя страница"""
-    return render(request, "home.html")
+    return render(
+        request,
+        "home.html",
+        context={
+            "new_item_text": request.POST.get("item_text", ""),
+        },
+    )

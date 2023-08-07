6bb4ad7d883bc256a2e2fd164c975b5e88fd9df2
ok_kir <clicktmin@gmail.com>
2023-08-07 17:44:45 +0400

    feat [tdd_with_python]: Refactored homepage views to use a template

--- book_club_training/tdd_with_python/superlists/lists/tests.py
+++ book_club_training/tdd_with_python/superlists/lists/tests.py
@@ -1,24 +1,10 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
     """Тест домашней страницы"""
 
-    def test_root_url_resolves_to_home_page_view(self):
-        """Тест: корневой url преобразуется в
-        представление домашней страницы"""
-        found = resolve("/")
-        self.assertEquals(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        """Тест: домашняя страница возвращает правильный html"""
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode("utf8")
-        self.assertTrue(html.startswith("<html>"))
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.endswith("</html>"))
+    def test_uses_home_template(self):
+        """Тест: используется домашний шаблон"""
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")

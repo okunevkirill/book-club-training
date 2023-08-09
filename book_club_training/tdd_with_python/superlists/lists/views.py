e456a482b25c10ce8a5f97a5fae0eed635f3bb82
ok_kir <clicktmin@gmail.com>
2023-08-09 19:44:54 +0400

    feat [tdd_with_python]: New URL + view for adding to existing lists. FT passes :-)

--- book_club_training/tdd_with_python/superlists/lists/views.py
+++ book_club_training/tdd_with_python/superlists/lists/views.py
@@ -8,14 +8,21 @@ def home_page(request):
     return render(request, "home.html")
 
 
-def view_list(request):
+def view_list(request, list_id):
     """Представление списка"""
-    items = Item.objects.all()
-    return render(request, "list.html", context={"items": items})
+    list_ = List.objects.get(id=list_id)
+    return render(request, "list.html", context={"list": list_})
 
 
 def new_list(request):
     """Новый список"""
     list_ = List.objects.create()
     Item.objects.create(text=request.POST["item_text"], list=list_)
-    return redirect("/lists/the-only-list-in-the-world/")
+    return redirect(f"/lists/{list_.id}/")
+
+
+def add_item(request, list_id):
+    """Добавить элемент"""
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST["item_text"], list=list_)
+    return redirect(f"/lists/{list_.id}/")
--- book_club_training/tdd_with_python/superlists/superlists/urls.py
+++ book_club_training/tdd_with_python/superlists/superlists/urls.py
@@ -4,5 +4,6 @@ from lists import views as lists
 urlpatterns = [
     re_path(r"^$", lists.home_page, name="home"),
     re_path(r"^lists/new$", lists.new_list, name="new_list"),
-    re_path(r"^lists/the-only-list-in-the-world/$", lists.view_list, name="view_list"),
+    re_path(r"^lists/(\d+)/$", lists.view_list, name="view_list"),
+    re_path(r"^lists/(\d+)/add_item$", lists.add_item, name="add_item"),
 ]

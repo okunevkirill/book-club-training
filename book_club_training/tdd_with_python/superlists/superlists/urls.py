b13f2684a9a40f0e25a1e992eead2f5837943ab1
ok_kir <clicktmin@gmail.com>
2023-08-09 17:37:39 +0400

    feat [tdd_with_python]: Add a URL to create a new list via POST

--- book_club_training/tdd_with_python/superlists/superlists/urls.py
+++ book_club_training/tdd_with_python/superlists/superlists/urls.py
@@ -2,6 +2,7 @@ from django.urls import re_path
 from lists import views as lists
 
 urlpatterns = [
-    re_path(r"^$", lists.home_page),
-    re_path(r"^lists/the-only-list-in-the-world/$", lists.view_list),
+    re_path(r"^$", lists.home_page, name="home"),
+    re_path(r"^lists/new$", lists.new_list, name="new_list"),
+    re_path(r"^lists/the-only-list-in-the-world/$", lists.view_list, name="view_list"),
 ]

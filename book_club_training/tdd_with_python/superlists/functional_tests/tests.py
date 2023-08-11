e2c81877727036033b696218aa053246b912af50
ok_kir <clicktmin@gmail.com>
2023-08-11 15:24:22 +0400

    feat [tdd_with_python]: FT's first steps for layout + styling

--- book_club_training/tdd_with_python/superlists/functional_tests/tests.py
+++ book_club_training/tdd_with_python/superlists/functional_tests/tests.py
@@ -115,3 +115,25 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn("Купить молоко", page_text)
 
         # Удовлетворенные, они оба ложатся спать
+
+    def test_layout_and_styling(self):
+        """Тест макета и стилевого оформления"""
+        # Эдит открывает домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Она замечает, что поле ввода аккуратно центрировано
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEqual(
+            input_box.location["x"] + input_box.size["width"] / 2, 512, delta=25
+        )
+
+        # Она начинает новый список и видит, что поле ввода там тоже
+        # аккуратно центрировано
+        input_box.send_keys("testing")
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: testing")
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEqual(
+            input_box.location["x"] + input_box.size["width"] / 2, 512, delta=10
+        )
